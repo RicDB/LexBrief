f0ebfabc8c21e86aee6a524c6cf237fd5974aee4
Riccardo DI BARI <riccardo.dibari@alten.it>
2024-05-22 15:58:46 +0200

    add pdf preview

--- lexbrief.client/src/app/preview-document/preview-document.component.ts
+++ lexbrief.client/src/app/preview-document/preview-document.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Pipe, PipeTransform } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import { DocumentDetailDto, DocumentsListService } from '../documents-list/documents-list.service';
 import { tap } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-preview-document',
@@ -19,14 +20,16 @@ export class PreviewDocumentComponent {
     this.getDocumentDetail(value)
   };
   document!: DocumentDetailDto;
+  preview: any;
 
-  constructor(private  service: DocumentsListService) {
+  constructor(private  service: DocumentsListService, private san: DomSanitizer) {
   }
 
   getDocumentDetail(id: number){
       this.service.getDocument(id).pipe(
         tap((document) => {
           this.document = document;
+          this.preview = this.san.bypassSecurityTrustResourceUrl(`data:application/pdf;base64,${document.content}`)
         })
       ).subscribe();    
   }
